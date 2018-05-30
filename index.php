e225e360d1d4a7ff3e9b06f6994a94dbfbac7aba
beweb-master <fondespierre.numerique@gmail.com>
2018-05-30 21:46:24 +0200

    ajout autoload des classes

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-
+spl_autoload_register(function ($class) {
+    if (file_exists('./core/' . $class . '.php')):
+        include './core/' . $class . '.php';
+    elseif (file_exists('./models/' . $class . '.php')):
+        include './models/' . $class . '.php';    
+    elseif (file_exists('./controllers/' . $class . '.php')):
+        include './controllers/' . $class . '.php';
+    elseif (file_exists('./dao/' . $class . '.php')):
+        include './dao/' . $class . '.php';
+    endif;
+});
