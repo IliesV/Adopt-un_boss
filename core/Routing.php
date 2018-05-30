a8b207000d871ccecd6ad7a82a2ff9bb2a845d46
beweb-master <fondespierre.numerique@gmail.com>
2018-05-30 23:47:40 +0200

    Initialisation du routing

---
+++ core/Routing.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * L'objet Routing va invoquer la methode 
+ * definie dans le fichier de configuration selectionnée grace à l'URI
+ * 
+ *
+ * @author loic
+ */
+class Routing {
+    
+}
