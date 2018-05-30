740dee253a131d834b7c91842d204a6f9076419c
beweb-master <fondespierre.numerique@gmail.com>
2018-05-30 23:52:19 +0200

    prise en charge du fichier de routage

--- core/Routing.php
+++ core/Routing.php
@@ -7,5 +7,17 @@
  * @author loic
  */
 class Routing {
+    /**
+     *
+     * @var Array Tableau associatif representant le fichier de configuration 
+     */
+    private $config;
     
+    function __construct() {
+        $this->config = json_decode(
+                file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/config/routing.json")
+                , true
+                );
+    }
+
 }
