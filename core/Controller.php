d39f28d08e43829879e7fd8b9397302bc7a6c39d
beweb-master <fondespierre.numerique@gmail.com>
2018-06-05 11:01:47 +0200

    prise en charge des inputs post et get

--- core/Controller.php
+++ core/Controller.php
@@ -1,27 +1,53 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Controller
  *
  * @author loic
  */
 abstract class Controller {
-    
+    /**
+     * Cette propriété correspond a la variable superglobale $_GET, 
+     * elle sera initialisée a la création d'un controlleur 
+     * 
+     * @var array
+     */
     private $get;
     
+    /**
+     * Cette propriété correspond a la variable superglobale $_POST, 
+     * elle sera initialisée a la création d'un controlleur
+     * 
+     * @var array 
+     */
     private $post;
     
     
+    /**
+     * Le constructeur sera invoqué a la création des objets heritant de cette classe
+     * il initialisera les propriétés avec les valeurs des superglobales.
+     * 
+     */
+    function __construct() {
+        $this->get = $_GET;
+        $this->post = $_POST;
+    }
+
+    /**
+     * retourne la propriété $get afin de la rendre disponible aux developpeurs
+     * souhaitant étendre cette classe
+     * 
+     * @return array
+     */
     protected function inputGet(){
         return $this->get;
     }
     
+    /**
+     * retourne la propriété $post afin de la rendre disponible aux developpeurs
+     * souhaitant étendre cette classe
+     * 
+     * @return array
+     */
     protected function inputPost() {
         return $this->post;
     }
